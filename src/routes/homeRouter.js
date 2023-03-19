9370cab0c2aa4623d0d6137a178e142ce5870c1c
Bruno Campos <campos.bruno98@gmail.com>
2023-03-19 13:53:27 -0300

    carrinho e detalhe-produto dinamicos

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -4,8 +4,8 @@ const homeController = require('../controllers/homeController')
 const router = express.Router();
 
 router.get('/', homeController.showHome)
-router.get('/carrinho', homeController.showCarrinho)
-router.get('/detalhe-produto', homeController.showDetalheProduto)
+router.get('/carrinho/:id', homeController.showCarrinho)
+router.get('/detalhe-produto/:id', homeController.showDetalheProduto)
 router.get('/finalizacao-da-compra', homeController.showFinalizacaoDaCompra)
 router.get('/listagem-produtos', homeController.showListagemProduto)
 router.get('/pagina-cadastro', homeController.showPaginaCadastro)
